e6d9bf6e125a296998381e7843111e7412864fd8
shivajigoud <shiva.vumma@gmail.com>
2023-01-12 18:25:40 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
-import { setupIonicReact } from '@ionic/react';
+import {
+  setupIonicReact,
+  IonApp,
+  IonPage,
+  IonHeader,
+  IonMenu,
+  IonContent,
+  IonToolbar,
+  IonTitle,
+} from '@ionic/react';
 import '@ionic/react/css/core.css';
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
 import router from './routes/userRouter';
 import './style.css';
 import FormContextProvider from './context/FormContext';
 setupIonicReact();
 export default function App() {
   return (
-    <FormContextProvider>
-      <RouterProvider router={router}></RouterProvider>
-    </FormContextProvider>
+    <IonApp>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Header</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <FormContextProvider>
+          <RouterProvider router={router}></RouterProvider>
+        </FormContextProvider>
+      </IonContent>
+    </IonApp>
   );
 }
